7d0cca2c2c00ac349f1eda909cf7c0ae473a04ac
Kreepeez <rijad.islamovic96@gmail.com>
2021-12-22 19:06:40 +0100

    fixed commit

---
+++ ShadowMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowMove : MonoBehaviour
+{
+    
+    int layerMask;
+
+    public GameObject blobShadow;
+
+    public float maxDistance;
+    public float minDistance;
+
+    void Start()
+    {
+        layerMask = LayerMask.GetMask("solid");
+    }
+
+    
+    void Update()
+    {
+        
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 15f, layerMask);
+
+        if (hit.collider != null)
+        {
+            blobShadow.SetActive(true);
+            blobShadow.transform.position = hit.point;
+            Debug.DrawLine(transform.position, hit.centroid, Color.yellow);
+
+
+             if(hit.distance <= maxDistance || hit.distance >= minDistance)
+             {
+                 float distanceRatio = minDistance / hit.distance;
+
+                if(distanceRatio > 1f)
+                {
+                    distanceRatio = 1f;
+                } else if(distanceRatio < 0.3f)
+                {
+                    distanceRatio = 0.3f;
+                }
+
+                blobShadow.GetComponent<Transform>().localScale = new Vector2(distanceRatio, distanceRatio);
+            }  
+        }
+        else blobShadow.SetActive(false);
+    }
+}
+        
+    
+
+
+
+
+
+
